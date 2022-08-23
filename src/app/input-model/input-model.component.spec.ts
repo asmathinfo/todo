a705fc39d4df190386e04e1d33f9efbe911a2b54
Buhary Asmath <asmath.info@gmail.com>
2022-08-23 23:00:52 +0530

    ngModel example

---
+++ src/app/input-model/input-model.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InputModelComponent } from './input-model.component';
+
+describe('InputModelComponent', () => {
+  let component: InputModelComponent;
+  let fixture: ComponentFixture<InputModelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InputModelComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InputModelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/input-model/input-model.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-model',
+  templateUrl: './input-model.component.html',
+  styleUrls: ['./input-model.component.css']
+})
+export class InputModelComponent implements OnInit {
+  name='Asmath';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
